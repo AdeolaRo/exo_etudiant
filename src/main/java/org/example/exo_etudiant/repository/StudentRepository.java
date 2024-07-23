13887a78cff8d61b1047b0ff0d522859a8631e6a
Abdel Chitou <char.wilsons@gmail.com>
2024-07-23 10:00:20 +0200

    commit 3.

---
+++ src/main/java/org/example/exo_etudiant/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package org.example.exo_etudiant.repository;
+
+
+import org.example.exo_etudiant.model.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+    Student findByFirstNameIgnoreCase(String firstName);
+    Student findByLastNameIgnoreCase(String lastName);
+}
